ffdf6c8b6f7d94273f76c19d5d837fb5d8948f4a
adzh001 <dzhumanazarova_a@auca.kg>
2022-03-22 21:37:42 +0600

    finish practice 5

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,183 +1,107 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  //task 1
-  int a = 10;
-  switch (a) {
-    case 10:
-      print("valid");
-      break;
+  //task 9
+  /*int sum1 = 0;
+  for (int i = 1; i < 101; i++) {
+    sum1 += i;
+  }
+  print(sum1);
 
-    default:
-      print("invalid");
-      break;
+  //task 10;
+  int num = 3;
+  String numToStr = num.toString();
+  String str1 = numToStr * 2;
+  String str2 = numToStr * 3;
+  print(num + int.parse(str2) + int.parse(str1));
+
+  //task 7;
+  stdout.write("Your name: ");
+  String? name = stdin.readLineSync();
+  print(name);
+
+  stdout.write("Your age: ");
+  int age = int.parse(stdin.readLineSync()!);
+  print(age);
+
+  stdout.write("Your address: ");
+  String? address = stdin.readLineSync();
+  print(address);
+
+  stdout.write("Your hobby: ");
+  String? hobby = stdin.readLineSync();
+  print(hobby); 
+  
+
+  //task 5;
+  for (int i = 0; i < 8; i++) {
+    print("Loading");
   }
 
-  //task 3
-  stdout.write("Enter the number from 1-4: ");
-  String res;
-  int num = int.parse(stdin.readLineSync()!);
-  switch (num) {
-    case 1:
-      res = "winter";
-      print(res);
-      break;
-    case 2:
-      res = "spring";
-      print(res);
-      break;
-    case 3:
-      res = "summer";
-      print(res);
-      break;
-    case 4:
-      res = "autumn";
-      print(res);
-      break;
+  //task 6;
+  int a = 5;
+  int b = 9;
+  int c = 5;
+  int d = a + b;
+  if (d == c) {
+    print(d);
+  } else
+    print(a * b * c);
 
-    default:
-      print("Invalid input");
-      break;
-  }
+  //task 1;
+  List list = [1, 2, 3, 4, 5, 6];
+  print(list.first);
+  print(list.last);
 
-  //task 4
+  // task 2;
+  int n = 123;
+  var a = n / 100;
+  var b = n / 10 % 10;
+  var c = n % 10;
+  print(a + b + c);
+
+  
+  // task 3;
+  int num3 = 345;
+
+  String res = num3.toString();
+  List array = res.split('');
+
+  int a = array.length;
+
+  double res2 = a / 2;
+  array.insert(res2.round() - 1, "0");
+  array.remove(res2 + 1);
+  print(array);
+*/
+// task 4;
   stdout.write("Enter the number from 1-7: ");
-  int day = int.parse(stdin.readLineSync()!);
-
-  List toDo = [];
-  toDo = [
-    "Проснуться",
-    "Почистить зубы",
-    "умыться",
-    "Пойти на учебу",
-    "Сделать дз"
-  ];
-  switch (day) {
+  int number = int.parse(stdin.readLineSync()!);
+  switch (number) {
     case 1:
-      toDo = [
-        "Проснуться",
-        "Почистить зубы",
-        "умыться",
-        "Пойти на учебу",
-        "Сделать дз"
-      ];
-      for (int i = 0; i < toDo.length; i++) {
-        print("$i, ${toDo[i]}");
-      }
+      print("Monday");
       break;
     case 2:
-      toDo = [
-        "Проснуться",
-        "Почистить зубы",
-        "умыться",
-        "Пойти на учебу",
-        "Сделать дз"
-      ];
-      for (int i = 0; i < toDo.length; i++) {
-        print("$i, ${toDo[i]}");
-      }
+      print("Tuesday");
       break;
     case 3:
-      toDo = [
-        "Проснуться",
-        "Почистить зубы",
-        "умыться",
-        "Пойти на учебу",
-        "Сделать дз"
-      ];
-      for (int i = 0; i < toDo.length; i++) {
-        print("$i, ${toDo[i]}");
-      }
+      print("Wednesday");
       break;
     case 4:
-      toDo = [
-        "Проснуться",
-        "Почистить зубы",
-        "умыться",
-        "Пойти на учебу",
-        "Сделать дз"
-      ];
-      for (int i = 0; i < toDo.length; i++) {
-        print("$i, ${toDo[i]}");
-      }
+      print("Thusrday");
       break;
     case 5:
-      toDo = [
-        "Проснуться",
-        "Почистить зубы",
-        "умыться",
-        "Пойти на учебу",
-        "Сделать дз"
-      ];
-      for (int i = 0; i < toDo.length; i++) {
-        print("$i, ${toDo[i]}");
-      }
+      print("Friday");
       break;
     case 6:
-      toDo = [
-        "Проснуться",
-        "Почистить зубы",
-        "умыться",
-        "Пойти на учебу",
-        "Сделать дз"
-      ];
-      for (int i = 0; i < toDo.length; i++) {
-        print("$i, ${toDo[i]}");
-      }
+      print("Saturday");
       break;
-
     case 7:
-      toDo = [
-        "Проснуться",
-        "Почистить зубы",
-        "умыться",
-        "Пойти на учебу",
-        "Сделать дз"
-      ];
-      for (int i = 0; i < toDo.length; i++) {
-        print("$i, ${toDo[i]}");
-      }
-      break;
-
-    default:
-      print("Please, enter the numbers from 1 to 7 only!");
-      break;
-  }
-
-  //task 2
-  /*stdout.write("Which language you prefer, Russian or English? ");
-  String? lang = stdin.readLineSync();
-
-  List ru = [
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятнца",
-    "Суббота",
-    "Воскресенье"
-  ];
-  List eng = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday"
-  ];
-
-  switch (lang) {
-    case "ru":
-      (print(ru));
-      break;
-
-    case "eng":
-      (print(eng));
+      print("Sunday");
       break;
 
     default:
       print("Out of usage");
       break;
-  } */
+  }
 }
